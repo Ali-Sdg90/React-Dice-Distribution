fe5d1e544f128efb08f45bbdaa190e6ca67f5270
Ali Sadeghi <ali909392@gmail.com>
2023-07-01 20:59:12 +0330

    Create base files

---
+++ dice-distribution/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Styles from "./App.module.css";
+
+const App = () => {
+    return (
+        <div className={Styles.app}>
+            <h1>Hi</h1>
+        </div>
+    );
+};
+
+export default App;
